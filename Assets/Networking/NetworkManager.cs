11891b883129bf5bbda60dc6033c88a59e4fa8e2
Jure Veler <jure.veler@gmail.com>
2015-03-31 22:41:55 +0200

    networkmanger

--- Assets/Networking/NetworkManager.cs
+++ Assets/Networking/NetworkManager.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class NetworkManager : MonoBehaviour {
 
 	// Use this for initialization
-	/*
+
 	void Start () 
 	{
-		PhotonNetwork.ConnectUsingSettings("0.1");
+		PhotonNetwork.ConnectUsingSettings ("0.1");
 	}
-	*/
 	
 	// Update is called once per frame
 	/*
