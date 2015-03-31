18d2c84f80a4e396506cd8b6ceff32069449d3ff
Henric Johansson <henric.johansson88@gmail.com>
2015-03-31 22:23:37 +0200

    Added player prefab and movement

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10f;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InputMovement();
+	}
+
+    void InputMovement()
+    {
+        Vector2 curVel = rb.velocity;
+        curVel.x = (float)(Input.GetAxis("Horizontal") * speed);
+        rb.velocity = curVel;
+    }
+}
