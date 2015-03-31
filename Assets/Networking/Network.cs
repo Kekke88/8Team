b6dacfc80cacc32ddb37eb4dc7319dc9ce555d8d
Henric Johansson <henric.johansson88@gmail.com>
2015-03-31 21:44:27 +0200

    Added Start method

--- Assets/Networking/Network.cs
+++ Assets/Networking/Network.cs
@@ -5,7 +5,7 @@ public class Network : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        PhotonNetwork.ConnectUsingSettings("0.1");
 	}
 	
 	// Update is called once per frame
