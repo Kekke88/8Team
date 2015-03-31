1c5555c0db414fa509caa435cab5532df9598a69
Jure Veler <jure.veler@gmail.com>
2015-03-31 22:51:16 +0200

    added code in networkmanager

--- Assets/Networking/NetworkManager.cs
+++ Assets/Networking/NetworkManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class NetworkManager : MonoBehaviour {
 
@@ -9,11 +10,45 @@ public class NetworkManager : MonoBehaviour {
 	{
 		PhotonNetwork.ConnectUsingSettings ("0.1");
 	}
-	
 	// Update is called once per frame
 	/*
 	void Update () {
 	
 	}
 	*/
+	private const string roomName = "RoomName";
+	private RoomInfo[] roomsList;
+	
+	void OnGUI()
+	{
+		if (!PhotonNetwork.connected)
+		{
+			GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		}
+		else if (PhotonNetwork.room == null)
+		{
+			// Create Room
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				PhotonNetwork.CreateRoom(roomName + Guid.NewGuid().ToString("N"), true, true, 5);
+			
+			// Join Room
+			if (roomsList != null)
+			{
+				for (int i = 0; i < roomsList.Length; i++)
+				{
+					if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+						PhotonNetwork.JoinRoom(roomsList[i].name);
+				}
+			}
+		}
+	}
+	
+	void OnReceivedRoomListUpdate()
+	{
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+	void OnJoinedRoom()
+	{
+		Debug.Log("Connected to Room");
+	}
 }
