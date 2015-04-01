32e12a2b2e0f7cb88de2ef29b4f52a05579fbd32
Henric Johansson <henric.johansson88@gmail.com>
2015-04-01 21:11:33 +0200

    Added lerp to player object

---
+++ Assets/Sync.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sync : MonoBehaviour {
+    private Vector3 trueLoc;
+    Quaternion trueRot;
+    PhotonView punView;
+
+	// Use this for initialization
+	void Start () {
+        punView.GetComponent<PhotonView>();
+	}
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.isReading)
+        {
+            if(!punView.isMine)
+            {
+                this.trueLoc = (Vector3)stream.ReceiveNext();
+            }
+        }
+        else
+        {
+            if(punView.isMine)
+            {
+                stream.SendNext(transform.position);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(!punView.isMine)
+        {
+            transform.position = Vector3.Lerp(transform.position, trueLoc, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, trueRot, Time.deltaTime * 5);
+        }
+	}
+}
