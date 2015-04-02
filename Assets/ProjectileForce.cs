03d16c1febcbfcb39304a9b80624ad17002cb026
Henric Johansson <henric.johansson88@gmail.com>
2015-04-02 19:35:59 +0200

    FeSt

---
+++ Assets/ProjectileForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileForce : MonoBehaviour {
+    private float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D projectileBody = GetComponent<Rigidbody2D>();
+        Transform transf = GetComponent<Transform>();
+
+        projectileBody.velocity = transf.forward * speed;
+        Debug.Log(transf.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
