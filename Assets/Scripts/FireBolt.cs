83613e2275aed7cf80919162ea38db25320225ca
Henric Johansson <henric.johansson88@gmail.com>
2015-04-06 21:48:26 +0200

    Projectile Base added

---
+++ Assets/Scripts/FireBolt.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBolt : Projectile {
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Transform))]
+public abstract class Projectile : MonoBehaviour {
+
+    public int speed = 10;
+    public int maxDistance = 100;
+    public int damage = 100;
+
+    public Rigidbody2D projectileBody;
+    public Transform transform;
+
+    private Vector3 basePosition;
+
+	protected virtual void Start () {
+        projectileBody = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+
+        //Get projectile base position
+        basePosition = transform.position;
+
+        
+        Debug.Log(basePosition);
+	}
+	
+	protected virtual void Update () {
+        projectileBody.MovePosition(projectileBody.position + new Vector2(1, 0)  * speed *
+            Time.deltaTime);
+
+        //Check if max distance has been reached
+
+        float distance = Vector3.Distance(basePosition, transform.position);
+
+        if(distance > maxDistance)
+        {
+            //Max distance reached, delete gameobject
+            Destroy(gameObject);
+        }
+	}
+}
